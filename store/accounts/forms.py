d614bc3e7bb381764f67552e9109ae0c1976ed71
Raymond McCarthy <ray92mc@gmail.com>
2021-02-19 15:42:59 +0000

    minutes wk3 document

--- store/accounts/forms.py
+++ store/accounts/forms.py
@@ -5,7 +5,7 @@ from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
     model = CustomUser
-    fields = UserCreationForm.Meta.fields + ('age',)
+    fields = UserCreationForm.Meta.fields + ('',)
 
 class CustomUserChangeForm(UserChangeForm):
     
