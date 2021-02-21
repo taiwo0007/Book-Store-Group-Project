75a1516d9c9a925c5fa13afc1ab38d9f1164ae26
Raymond McCarthy <ray92mc@gmail.com>
2021-02-21 22:13:44 +0000

    manager customer

--- store/accounts/models.py
+++ store/accounts/models.py
@@ -2,4 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    age = models.PositiveIntegerField(null=True, blank=True)
+    is_customer = models.BooleanField(default=False)
+    is_manager = models.BooleanField(default=False)
+
+class Customer(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
+
+    def __str__(self):
+        return self.user.username
