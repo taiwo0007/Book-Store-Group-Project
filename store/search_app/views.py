8620bbf8d2861f14ecee55bd0addc621ad5e5a32
Raymond McCarthy <ray92mc@gmail.com>
2021-03-23 17:56:02 +0000

    change

--- store/search_app/views.py
+++ store/search_app/views.py
@@ -4,7 +4,7 @@ from django.db.models import Q
 
 def searchResult(request):
     books = None
-    query = None
+    query = None 
     if 'q' in request.GET:
         query = request.GET.get('q')
         books = Book.objects.all().filter(Q(title__contains=query) | Q(publisher__contains=query) | Q(iban__contains=query) | Q(author__first_name__contains=query) | Q(author__last_name__contains=query))
