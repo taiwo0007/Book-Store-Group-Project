c9c62fee53b1567c02f6cde5080af2e1c7694c90
Raymond McCarthy <ray92mc@gmail.com>
2021-03-31 12:27:36 +0100

    total sales

--- store/dashboard/views.py
+++ store/dashboard/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from order.models import Order
+from django.db.models import Sum, DecimalField
+from django.db.models.functions import Cast
 
 def manager_dashboard(request):
     return render(request, "dashboard/manager_dashboard.html")
@@ -9,3 +11,11 @@ def orders_list(request):
     orders = Order.objects.all()
     context = {"orders" : orders}
     return render(request, "dashboard/order_list.html", context)
+
+def reportsView(request):
+    return render(request, 'dashboard/reports.html')
+
+def sales_report(request):
+    total_sales = Order.objects.aggregate(my_sum=Cast(Sum('total'), DecimalField(max_digits=30, decimal_places=2)))
+    context = {'total_sales':total_sales}
+    return render(request, 'dashboard/sales.html', context)
