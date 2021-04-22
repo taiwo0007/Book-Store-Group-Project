20ec8e32134979890d82893b8411c3df444e6908
Taiwo Obadare <taiwo.obadare@gmail.com>
2021-04-22 12:37:28 +0100

    underline when hover on a number of topnavbar items

--- store/shop/context_processors.py
+++ store/shop/context_processors.py
@@ -1,6 +1,7 @@
 from .models import Category
 from accounts.models import UserProfile
 from vouchers.models import Voucher
+from django.utils.timezone import datetime
 
 def menu_links(request):
     links = Category.objects.all()
@@ -24,6 +25,7 @@ def annouceFunction(request):
     anouncemnets = Voucher.objects.filter(anounce=True)
     anouncemnet = anouncemnets.last()
     print(anouncemnets)
+  
     for i in anouncemnets:
         print(i)
 
