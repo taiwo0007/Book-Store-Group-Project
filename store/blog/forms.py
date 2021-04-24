40de1f7a1b70b3393443e2a3a72931c407d74a70
Taiwo Obadare <X00167646@myTUDublin.ie>
2021-04-24 11:33:59 +0000

    Deleted store

--- store/blog/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import Blog
-
-
-class BlogForm(ModelForm):
-    class Meta:
-        model = Blog
-        fields = ['title', 'body', 'image', 'book_blog']
--- store/contact/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-
-class ContactForm(forms.Form):
-    firstname = forms.CharField()
-    lastname = forms.CharField()
-    email = forms.EmailField()
-    subject = forms.CharField(widget=forms.Textarea)
