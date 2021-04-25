80f84d10cd9bb3849f567fdfe18ee2a9d28bd01e
Vishvajit Sundarrajan <X00162597@myTUDublin.ie>
2021-04-25 08:39:40 +0100

    search filters

--- search_app/views.py
+++ search_app/views.py
@@ -7,5 +7,5 @@ def searchResult(request):
     query = None
     if 'q' in request.GET:
         query = request.GET.get('q')
-        books = Book.objects.all().filter(Q(title__contains=query) | Q(publisher__contains=query) | Q(iban__contains=query) | Q(author__first_name__contains=query) | Q(author__last_name__contains=query))
+        books = Book.objects.all().filter(Q(title__contains=query) | Q(publisher__contains=query) | Q(iban__contains=query) | Q(author__first_name__contains=query) | Q(author__last_name__contains=query) | Q(category__name__contains=query) | Q(author__full_name__contains=query))
     return render(request, 'search.html',{'query':query,'books':books})
